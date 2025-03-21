6972304ad0991b181f6e174f35ebe3555380e592
Anastasiia <enfenim.dinan@gmail.com>
2025-03-21 19:10:13 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anastasiia Kotova on 21.03.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
